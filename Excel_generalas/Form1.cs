72c3c39f050bc665c502adbbac4d9a13626c8b8d
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-10-04 12:54:10 +0200

    ok

--- Excel_generalas/Form1.cs
+++ Excel_generalas/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
-
+using System.Data.Entity.Migrations.Model;
 
 namespace Excel_generalas
 {
@@ -17,11 +17,47 @@ namespace Excel_generalas
     {
         RealEstateEntities context = new RealEstateEntities(); // ORM objektum példányosítása
         List<Flat> Flats; // Flat tipusú elemekből álló listára mutató referencia
+
+        Excel.Application xlApp; // A microsoft excel alkalmazás
+        Excel.Workbook xlWB; // A létrehozott munkafüzet
+        Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application(); //Excel elindítása és az applikáció objektum betöltése
+                xlWB = xlApp.Workbooks.Add(Missing.Value); // Új munkafüzet
+                xlSheet = xlWB.ActiveSheet; // Új munkalap
+                /*CreateTable(); //Tábla létrehozása*/
+                xlApp.Visible = true; //Control átadása a felhasználónak
+                xlApp.UserControl = true; //Control átadása a felhasználónak
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+            
+        }
+
+       /* private void CreateTable()
+        {
+            throw new NotImplementedException();
+        }*/
 
         private void LoadData()
         {
