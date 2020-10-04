519f04f8880332e0f5d72cbc9bf25e08c0353654
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-10-04 21:21:08 +0200

    ok

--- Excel_generalas/Form1.cs
+++ Excel_generalas/Form1.cs
@@ -30,14 +30,16 @@ namespace Excel_generalas
             CreateExcel();
         }
 
-        private void CreateExcel()
+
+
+         private void CreateExcel()
         {
             try
             {
                 xlApp = new Excel.Application(); //Excel elindítása és az applikáció objektum betöltése
                 xlWB = xlApp.Workbooks.Add(Missing.Value); // Új munkafüzet
                 xlSheet = xlWB.ActiveSheet; // Új munkalap
-                /*CreateTable(); //Tábla létrehozása*/
+                CreateTable(); //Tábla létrehozása*/
                 xlApp.Visible = true; //Control átadása a felhasználónak
                 xlApp.UserControl = true; //Control átadása a felhasználónak
             }
@@ -54,14 +56,76 @@ namespace Excel_generalas
             
         }
 
-       /* private void CreateTable()
+       
+        
+        private void CreateTable()
         {
-            throw new NotImplementedException();
-        }*/
+            string[] headers = new string[] //tömb amely tartalmazza a tábla fejléceit
+           { "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"
+           };
+            for (int i = 0; i < headers.Length; i++)
+            {
+              xlSheet.Cells[1, i + 1] = headers[i];
+            }
+           
 
+            object[,] values = new object[Flats.Count, headers.Length];
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+        }
+
+        
+        
         private void LoadData()
         {
             Flats = context.Flats.ToList(); //Adattábla memóriába való másolása
         }
+
+        
+        
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+        
+    
+    
+    
+    
     }
 }
