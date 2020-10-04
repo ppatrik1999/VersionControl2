5c5deced5feac592f9860d5b6749688a6ac8d5b6
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-10-04 12:37:17 +0200

    ok

--- Excel_generalas/Form1.cs
+++ Excel_generalas/Form1.cs
@@ -13,16 +13,17 @@ namespace Excel_generalas
 {
     public partial class Form1 : Form
     {
-       
-
-
-
+        RealEstateEntities context = new RealEstateEntities(); // ORM objektum példányosítása
+        List<Flat> Flats; // Flat tipusú elemekből álló listára mutató referencia
         public Form1()
         {
             InitializeComponent();
-          
+            LoadData();
         }
 
-       
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList(); //Adattábla memóriába való másolása
+        }
     }
 }
