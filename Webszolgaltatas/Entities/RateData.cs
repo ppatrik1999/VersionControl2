dfa7cd36579f2fdf3d42a795c2af9d530f12e472
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-10-17 20:03:36 +0200

    ok

---
+++ Webszolgaltatas/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webszolgaltatas.Entities
+{
+    class RateData
+    {
+        public DateTime Date;
+        public string Currency;
+        public decimal Value;
+    }
+}
