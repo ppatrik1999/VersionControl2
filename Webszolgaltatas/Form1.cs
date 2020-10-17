69b732c28fdba79a6bae2b16da80d3a858ddd51e
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-10-17 19:54:53 +0200

    ok

---
+++ Webszolgaltatas/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Webszolgaltatas.MnbServiceReference;
+
+namespace Webszolgaltatas
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            GetExchangeRates();
+        }
+
+        private void GetExchangeRates()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+
+
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+        }
+    }
+}
