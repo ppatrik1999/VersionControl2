53d9a09a057befb26de60a45ef63ffbb2c57c4f2
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-10-24 12:10:15 +0200

    ok

---
+++ Mikroszimulácuó/Entities/Gender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulácuó.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+    }
+
+    public class Person
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+        public double BirthProbability { get; set; }
+        public double DeathProbability { get; set; }
+
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+
+
+
+}
